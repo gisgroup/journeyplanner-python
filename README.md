3864b636836a15b84a639b0965a477e8eca47fed
zachasme <zachasme@gmail.com>
2015-03-25 22:29:07 +0100

    Move services into separate modules

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rejseplanen-python
 Python client for Rejseplanens API
+
+## Usage
+```
+import rejseplanen
+
+client = rejseplanen.CLIENT('username', 'password')
+
+client.location('elmegade 5 københavn')
+```
--- rejseplanen/requester.py
+++ rejseplanen/requester.py
@@ -7,17 +7,18 @@ Helper for performing authenticated http requests
 from urllib import request
 from urllib.parse import urlencode
 
+
 class Requester:
-  def __init__(self, base_url, username, password):
-    self.base_url = base_url
-    passman = request.HTTPPasswordMgrWithDefaultRealm()
-    passman.add_password(None, base_url, username, password)
-    auth_handler = request.HTTPBasicAuthHandler(passman)
-    self.opener = request.build_opener(auth_handler)
+    def __init__(self, base, username, password):
+        self.base = base
+        manager = request.HTTPPasswordMgrWithDefaultRealm()
+        manager.add_password(None, self.base, username, password)
+        handler = request.HTTPBasicAuthHandler(manager)
+        self.opener = request.build_opener(handler)
 
-  def get(self, path, **query):
-    tuples = sorted(query.items(), key = lambda x:x[0] )
-    querystring = urlencode(tuples)
-    url = '%s%s?%s' % (self.base_url, path, querystring)
+    def get(self, path, **query):
+        tuples = sorted(query.items(), key=lambda x: x[0])
+        querystring = urlencode(tuples)
+        url = '%s%s?%s' % (self.base, path, querystring)
 
-    return self.opener.open(url)
+        return self.opener.open(url)
